59060d3f3d70803d6359eaba4eb5a6ec1a67f5a2
Nemanja Grubor <grubor.nemanja@gmail.com>
2022-11-25 19:48:25 +0100

    commit

---
+++ dialogue.js
@@ -0,0 +1,18 @@
+const prompts = [
+  ["hi", "hello"],
+  ["tell me about the cake"],
+  ["goodbye"]
+
+]
+
+const replies = [
+  ["HA! I knew someone was alive in here", "AH! Oh. My. God. You look terribl-- ummm... good. Looking good, actually."],
+  ["The CAKE is delicious. It can't be a lie, as someone would say."],
+  ["I'm going to give you a sporting chance and turn off the neurotoxin. I'm joking. Of course. Goodbye."]
+
+]
+
+const alternative = [
+  "I don't understand."
+]
+
---
+++ index.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const inputField = document.getElementById("input");
+  inputField.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      let input = inputField.value;
+      inputField.value = "";
+      output(input);
+    }
+  });
+});
+
+function output(input) {
+  let product;
+
+  let text = input.toLowerCase().replace(/[\d]/, "").trim();
+
+  if (compare(prompts, replies, text)) { 
+    product = compare(prompts, replies, text);
+  } 
+   else {
+    product = alternative[Math.floor(Math.random() * alternative.length)];
+  }
+
+  addChat(input, product);
+}
+
+function compare(promptsArray, repliesArray, string) {
+  let reply;
+  let replyFound = false;
+  for (let x = 0; x < promptsArray.length; x++) {
+    for (let y = 0; y < promptsArray[x].length; y++) {
+      if (promptsArray[x][y] === string) {
+        let replies = repliesArray[x];
+        reply = replies[Math.floor(Math.random() * replies.length)];
+        replyFound = true;
+        break;
+      }
+    }
+    if (replyFound) {
+      break;
+    }
+  }
+  return reply;
+}
+
+function addChat(input, product) {
+  const messagesContainer = document.getElementById("messages");
+
+  let userDiv = document.createElement("div");
+  userDiv.id = "user";
+  userDiv.className = "user response";
+  userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
+  messagesContainer.appendChild(userDiv);
+
+  let botDiv = document.createElement("div");
+  let botImg = document.createElement("img");
+  let botText = document.createElement("span");
+  botDiv.id = "bot";
+  botImg.src = "wheatley.png";
+  botImg.className = "avatar";
+  botDiv.className = "bot response";
+  botText.innerText = "Typing...";
+  botDiv.appendChild(botText);
+  botDiv.appendChild(botImg);
+  messagesContainer.appendChild(botDiv);
+  messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+
+  setTimeout(() => {
+    botText.innerText = `${product}`;
+    textToSpeech(product)
+  }, 2000
+  )
+
+}
